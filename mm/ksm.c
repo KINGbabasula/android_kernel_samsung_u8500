007c61002254a98a8c84ba585d0f26b756ce98ae
zwliew <zhaoweiliew@gmail.com>
2014-06-01 06:57:46 -0700

    ksm: enable deferred timer
    Change-Id: I3e4745206a1333fb67e0473573cc44d78c1895c7

--- mm/ksm.c
+++ mm/ksm.c
@@ -189,6 +189,9 @@ static unsigned int ksm_thread_pages_to_scan = 100;
 /* Milliseconds ksmd should sleep between batches */
 static unsigned int ksm_thread_sleep_millisecs = 20;
 
+/* Boolean to indicate whether to use deferred timer or not */
+static bool use_deferred_timer = true;
+
 #define KSM_RUN_STOP	0
 #define KSM_RUN_MERGE	1
 #define KSM_RUN_UNMERGE	2
